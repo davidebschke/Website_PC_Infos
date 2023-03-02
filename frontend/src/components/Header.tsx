65bc3ff3348bdcb742362e938f152e57b266c0bb
davidebschke <davidebschke8@gmail.com>
2023-03-02 19:11:02 +0100

    add Features list to Header.tsx

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,6 +3,7 @@ export default function Header (){
     return(
         <>
             Header
+            Folgende Features kommen hier : Logo (Anklickbar zur Startseite) , Navigationsleiste, Suchfeld
         </>
     )
 }
